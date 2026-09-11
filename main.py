804c5cec8285f2c9abc9037f7ac6e3e8fdf0693f
supremiere <ninemailz@gmail.com>
2026-09-11 16:01:46 +0900

    Enable combo operation reports in rune stats

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import rune_stats_bootstrap  # noqa: F401
 import rune_stats_patch  # noqa: F401
 import rune_stats_hotfix  # noqa: F401
+import rune_stats_combo_patch  # noqa: F401
 import legacy_main  # noqa: F401
