800df0cd0877a9f4043b023528fee17bc45307e9
supremiere <ninemailz@gmail.com>
2026-09-11 16:03:47 +0900

    Hide rune stats one-line summary

--- main.py
+++ main.py
@@ -2,4 +2,5 @@ import rune_stats_bootstrap  # noqa: F401
 import rune_stats_patch  # noqa: F401
 import rune_stats_hotfix  # noqa: F401
 import rune_stats_combo_patch  # noqa: F401
+import rune_stats_cleanup  # noqa: F401
 import legacy_main  # noqa: F401
