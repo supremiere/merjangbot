4fd16c16e278f5370d10c3f3245cf0e2b61e2176
supremiere <ninemailz@gmail.com>
2026-09-22 11:18:14 +0900

    Use local abyss scheduler instead of Moblife

--- bootstrap.py
+++ bootstrap.py
@@ -2,14 +2,14 @@
 from discord_bot.bot import MerjangBot
 from sites.erinndata.runes import RuneStatsService
 from sites.http import HttpClient
-from sites.moblife.abyss import AbyssService
+from services.abyss import LocalAbyssService
 from sites.moblife.abyss_ranking import AbyssRankingService
 from sites.moblife.client import MoblifeClient
 from sites.official.maintenance import OfficialMaintenanceService
 from sites.moblife.market import MarketService
 from sites.moblife.sheets import SheetService
 from sites.official.notices import OfficialNotices
-from storage.abyss import AbyssRepository
+from storage.abyss import AbyssRepository, AbyssScheduleRepository
 from storage.abyss_ranking import AbyssRankingRepository
 from storage.database import Database
 from storage.notices import NoticeRepository
@@ -27,7 +27,7 @@ def create_bot(settings):
         database=database,
         rune_stats_service=RuneStatsService(http, RuneStatsRepository(database)),
         official=OfficialNotices(http),
-        abyss_service=AbyssService(moblife),
+        abyss_service=LocalAbyssService(AbyssScheduleRepository(database)),
         abyss_ranking_service=AbyssRankingService(moblife, AbyssRankingRepository(database)),
         market_service=MarketService(moblife),
         sheet_service=SheetService(moblife),
